4b566820e861fa4765cac51944db8ee3339521fc
Tushar Negi <negitushar81@gmail.com>
2024-10-03 17:49:14 +0530

    lenis with html and js

--- lenis with html & js/script.js
+++ lenis with html & js/script.js
@@ -1,15 +1,47 @@
-const lenis = new Lenis()
+const lenis = new Lenis();
 
 function raf(time) {
-  lenis.raf(time)
-  requestAnimationFrame(raf)
+  lenis.raf(time);
+  requestAnimationFrame(raf);
 }
 
-requestAnimationFrame(raf)
+requestAnimationFrame(raf);
 
+document.querySelectorAll(".elem").forEach((elem) => {
+  let img = elem.querySelector("img");
+  let tl = gsap.timeline();
 
-document.querySelector('.elem').forEach(elem => {
-  let img = elem.querySelector('img')
-  gsap.timeline()
-  .to()
-})
+  let xTransform = gsap.utils.random(-100, 100);
+
+  tl.set(
+    img,
+    {
+      transformOrigin: `${xTransform < 0 ? 0 : "100%"}`,
+    },
+    "start"
+  )
+    .to(
+      img,
+      {
+        scale: 0,
+        ease: "none",
+        scrollTrigger: {
+          trigger: elem,
+          start: "top top",
+          end: "bottom top",
+          scrub: true,
+        },
+      },
+      "start"
+    )
+    .to(elem, {
+      xPercent: xTransform,
+      ease: "none",
+      scrollTrigger: {
+        trigger: img,
+        start: "top bottom",
+        end: "bottom top",
+        scrub: true,
+      },
+    });
+});
